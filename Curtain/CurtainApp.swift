b2d6e4cab48a190cd5991ef1fe61f9d6bb847ee6
Toan Phung <toan.phung@proteo.info>
2025-08-02 11:02:00 +0100

    Initial Commit

---
+++ Curtain/CurtainApp.swift
@@ -0,0 +1,32 @@
+//
+//  CurtainApp.swift
+//  Curtain
+//
+//  Created by Toan Phung on 02/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CurtainApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CurtainTests/CurtainTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurtainTests.swift
+//  CurtainTests
+//
+//  Created by Toan Phung on 02/08/2025.
+//
+
+import Testing
+@testable import Curtain
+
+struct CurtainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
